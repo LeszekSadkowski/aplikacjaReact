9f12138cfd7e37b9579f370cb672e95822b62450
LeszekSadkowski <leszek.sadkowski@gmail.com>
2018-04-25 12:48:50 +0200

    przed wyborem stylu

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -6,17 +6,27 @@ class Invite extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            name:'',
-            who:'',
-            where:'',
-            when:''
+            name:this.props.info.name,
+            who:this.props.info.who,
+            where:this.props.info.where,
+            when:this.props.info.when,
+            option:this.props.info.option,
+            month:this.props.info.month
         }
     }
+    print=()=>{
+        window.print();
+
+    }
     render(){
+        console.log(this.props)
         return (
         <div id='inviteCard'>
             <div id='invNames' > {this.state.name} </div>
-            <div id='invInformation'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea nobis voluptatem dolorem, mollitia hic dolores optio labore. Consequatur perspiciatis molestiae ratione numquam blanditiis quia! Quis blanditiis illum sequi aliquam recusandae.</div>
+            <div id='invInformation'>{`Lorem ${this.state.who}ipsum dolor sit amet, consectetur ${this.state.where}adipisicing elit. Ea nobis voluptatem dolorem, mollitia hic dolores optio labore. Consequatur perspiciatis molestiae ratione numquam blanditiis quia! Quis blanditiis illum sequi aliquam recusandae.`}</div>
+            <div className="btnAnim">
+                 <div className="example-screen inviteBtn" onClick={this.print}  >print</div>
+            </div>
         </div>
         )
     }
@@ -31,7 +41,8 @@ class Form extends React.Component{
             where:'',
             when:'',
             option:this.props.option,
-            invite:false
+            invite:false,
+            month:null
         }
     }
     handleChange = (e, keyState) => {
@@ -40,12 +51,34 @@ class Form extends React.Component{
         this.setState(tempState)
     }
     handleClick=()=>{
+        let empty=true;
+        let wrgDate=true;
+        let whenEl= this.state.when.split('-');
+        const curYear= new Date().getFullYear();
+        const curMonth= new Date().getMonth();
+        const currDay= new Date().getDate();
+        const monthsArr=['January','February','March','April','May','June','July','August','September','October','November','December']
+        let monthUse=parseInt(whenEl[1]);
+        const monthName=monthsArr[monthUse-1];
+        if(this.state.name.length!=0||this.state.who.length!=0||
+            this.state.name.where!=0||this.state.when.length!=0){
+            empty=false;
+        }
+        if(whenEl[0]>=curYear && whenEl[1]>=curMonth && whenEl[2]>=currDay){
+            wrgDate=false;
+        }else{
+            alert('please fill out the form')
+        }
 
-        this.setState({
+        if (empty==false && wrgDate==false){
+           this.setState({
+            month:monthName,
             invite:true
-        })
-        console.log(this.state);
 
+            })
+        }else{
+            alert('date cannot be past')
+        }
     }
 
 
@@ -83,14 +116,14 @@ class Form extends React.Component{
                         value={this.state.when}/>
             </div>
             <div>
-                <button type="" id='formBtn'
+                <button  id='formBtn'
                         onClick={this.handleClick} >save</button>
             </div>
         </div>)
         if(this.state.invite){
             inviteEvent=<Invite info={this.state}/>
         }
-        console.log(this.state)
+
         return(
             <div>
                  {inviteEvent}
@@ -98,15 +131,33 @@ class Form extends React.Component{
         )
     }
 }
+class ChoseButton extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            option:this.props.option
+        }
+    }
+    handleClick=(e, props)=>{
+        console.log(this.state.option);
+        this.props.action(e,props)
+    }
+    render(){
+       return( <div>
+        <button className="page-button" onClick={e=>this.handleClick(e,this.state.option)}>{this.props.option}</button>
+        </div>
+       )
+    }
+}
 class View extends React.Component{
     handleClickView=(e, myEvent)=>{
-        console.log('work');
         this.props.action(e,myEvent)
     }
     render(){
         return(<div className="full-page">
-        <div><button className="page-button" onClick={e=>this.handleClickView(e,'wedding')}>wedding</button></div>
-        <div><button className="page-button" onClick={e=>this.handleClickView(e,'birth')}>birthday</button></div>
+        <ChoseButton option='wedding' action={this.handleClickView}/>
+        <ChoseButton option='birthday' action={this.handleClickView}/>
+
     </div>)
     }
 }
@@ -121,11 +172,12 @@ class App extends React.Component{
     }
 
     handleClick=(e, myEvent)=>{
-        console.log('work');
         this.setState({
             event:true,
             option: myEvent
-        })
+        });
+        console.log(myEvent);
+
     }
 
     render(){
